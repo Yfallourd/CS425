f7544034e2e35f0ee34fabde04742944b4c81507
root <root@localhost.localdomain>
2017-12-04 03:01:24 -0600

    Finished

--- targetapp.py
+++ targetapp.py
@@ -19,6 +19,8 @@ def generateID(typetoken):
 def redirect_to_main():
     redirect_to_main = redirect('/main')
     response = app.make_response(redirect_to_main)
+    response.set_cookie('auth_user', value='', expires=0)  # Remove cookies
+    response.set_cookie('employee', value='', expires=0)  # Remove cookies
     return response
 
 
@@ -90,6 +92,13 @@ def cartpage():
     return render_template('cart.html', cart=cart)
 
 
+@app.route('/checkout')
+def checkout():
+    if "auth_user" in request.cookies:
+        cursor = connection.cursor()
+        custID = request.cookies["auth_user"]
+        return "NOT IMPLEMENTED"
+
 @app.route('/customer/order')
 def orderpage():
     if "auth_user" in request.cookies:
@@ -121,8 +130,6 @@ def signinpage():
             and password = :password
         '''
         cursor.execute(querystr, {"username": payload["inputUsername"], "password":payload["inputPassword"]})
-        #if cursor.rowcount == 0:
-        #    return "USER NOT FOUND"
         for id in cursor:
             userid = id[0]
         redirect_to_main = redirect('/main')
@@ -195,6 +202,69 @@ def signuppage():
         return response
 
 
+@app.route('/employee/signup', methods=['GET', 'POST'])
+def esignuppage():
+    if request.method == "GET":
+        return render_template('customer_sign_up.html')
+    if request.method == "POST":
+        # SIGN UP LOGIC
+        i = 0
+        payload = request.form
+        cursor = connection.cursor()
+        querystr = '''
+        select username from account
+        where username = '{0}'
+        '''.format(payload["inputCustomerUsername"])
+        cursor.execute(querystr)
+        for id in cursor:
+            i += 1
+        if not i == 0:
+            return "USERNAME ALREADY EXISTS"
+        cursor = connection.cursor()
+        userid = generateID("u")
+        addressid = generateID("a")
+        querystr = '''insert into customers 
+                values (:userid,
+                :fname,
+                :mname,
+                :lname,
+                :phone
+            )        
+        '''
+        cursor.execute(querystr, {"userid": userid, "fname": payload["inputFName"], "mname": payload["inputMName"],
+                                  "lname": payload["inputLName"], "phone": payload["inputPhone"]})
+        connection.commit()
+        cursor = connection.cursor()
+        querystr = '''insert into account 
+                values (:password,
+                :username,
+                :userid,
+                :email
+            )        
+        '''
+        cursor.execute(querystr, {"password": payload["inputPassword"], "username": payload["inputCustomerUsername"],
+                                  "userid": userid, "email": payload["inputEmail"]})
+        connection.commit()
+        cursor = connection.cursor()
+        querystr = '''insert into address 
+                        values (:addressid,
+                        :a1,
+                        :a2,
+                        :city,
+                        :code,
+                        :state,
+                        :userid
+                    )        
+                '''
+        cursor.execute(querystr, {"addressid": addressid, "a1": payload["inputAddress1"],
+                                  "a2": payload["inputAddress2"], "city": payload["inputCity"],
+                                  "code": payload["inputCode"], "state": payload["inputState"], "userid": userid})
+        connection.commit()
+        redirect_to = redirect('customer/signup/success')
+        response = app.make_response(redirect_to)
+        return response
+
+
 @app.route('/customer/signup/success')
 def successpage():
     return render_template('signup_success.html')
@@ -202,10 +272,11 @@ def successpage():
 
 @app.route('/test')
 def test():
-    redirect_to_main = redirect('/main')
-    response = app.make_response(redirect_to_main)
-    response.set_cookie('auth_user', value='test')  # Add the username arg
-    return response
+    cursor = connection.cursor()
+    querystr = "select * from account"
+    cursor.execute(querystr)
+    stores = [x for x in cursor]
+    return render_template('stores.html', stores=stores)
 
 
 @app.route('/customer/account')
@@ -217,40 +288,47 @@ def accountpage():
 def accountUpdate():
     if request.method == 'POST':
         # UPDATE LOGIC
-        payload = request.form
-        cursor = connection.cursor()
-        querystr = '''
-                select username from account
-                where username = {0}
-                '''.format(payload["username"])
-        cursor.execute(querystr)
-        cursor = connection.cursor()
-        querystr = '''insert into customers
-                        values ({0},
-                        {1},
-                        {2},
-                        {3},
-                        {4},
-                    )        
-                '''.format(payload["inputCustomerUsername"], payload["inputFName"], payload["inputMName"],
-                           payload["inputLName"], payload["inputPhone"])
-        cursor.execute(querystr)
-        cursor = connection.cursor()
-        querystr = '''insert into address 
-                        values ({0},
-                        {1},
-                        {2},
-                        {3},
-                        {4},
-                        {5},
-                        {6}
-                    )        
-                '''.format(ADDRESSID, payload["inputAddress1"], payload["inputAddress2"], payload["inputCity"],
-                           payload["inputState"], payload["inputCode"], USERID)
-        cursor.execute(querystr)
-        redirect_to_main = redirect('/main')
-        response = app.make_response(redirect_to_main)
-        return response
+        if "auth_user" in request.cookies:
+            custID = request.cookies["auth_user"]
+            payload = request.form
+            cursor = connection.cursor()
+            querystr = '''update customers 
+                    set first_name = :fname,
+                    middle_name = :mname,
+                    last_name = :lname,
+                    phone_number = :phone
+                    where user_id = :userid    
+            '''
+            cursor.execute(querystr, {"userid": custID, "fname": payload["inputFName"], "mname": payload["inputMName"],
+                                      "lname": payload["inputLName"], "phone": payload["inputPhone"]})
+            connection.commit()
+            cursor = connection.cursor()
+            querystr = '''update account 
+                    set password = :password,
+                    username = :username,
+                    email = :email
+                    where user_id = :userid        
+            '''
+            cursor.execute(querystr, {"password": payload["inputPassword"], "username": payload["inputCustomerUsername"],
+                                      "userid": custID, "email": payload["inputEmail"]})
+            connection.commit()
+            cursor = connection.cursor()
+            querystr = '''update address 
+                            set address_line_1 = :a1,
+                            address_line_2 = :a2,
+                            city = :city,
+                            zipcode = :code,
+                            state = :state
+                            where user_id = :userid        
+                    '''
+            cursor.execute(querystr, {"a1": payload["inputAddress1"],
+                                      "a2": payload["inputAddress2"], "city": payload["inputCity"],
+                                      "code": payload["inputCode"], "state": payload["inputState"], "userid": custID})
+            connection.commit()
+            redirect_to_main = redirect('/main')
+            response = app.make_response(redirect_to_main)
+            return response
+        return "PLEASE SIGN IN"
 
 
 @app.route('/employee/signin', methods=['GET', 'POST'])
@@ -263,13 +341,13 @@ def empSignin():
         userid = None
         cursor = connection.cursor()
         querystr = '''
-            select user_id from account
-            where username = {0}
-            and password = {1}
-        '''.format(payload["inputUsername"], payload["inputPassword"])
-        cursor.execute(querystr)
+                    select user_id from account
+                    where username = :username
+                    and password = :password
+                '''
+        cursor.execute(querystr, {"username": payload["inputUsername"], "password": payload["inputPassword"]})
         for id in cursor:
-            userid = id
+            userid = id[0]
         redirect_to_main = redirect('/main')
         response = app.make_response(redirect_to_main)
         response.set_cookie('auth_user', value='')  # Add the username arg
@@ -279,35 +357,42 @@ def empSignin():
 
 @app.route('/employee/product')
 def productCheck():
-    #if ("employee" in request.cookies) & (request.cookies["employee"] == "True"):
-    return render_template("employee_check_product.html")
-    #else:
-        #return "FORBIDDEN"
+    if ("employee" in request.cookies) & (request.cookies["employee"] == "True"):
+        cursor = connection.cursor()
+        querystr = "select * from product"
+        cursor.execute(querystr)
+        products = [x for x in cursor]
+        return render_template("employee_check_product.html", products=products)
+    else:
+        return "FORBIDDEN"
 
 
 @app.route('/employee/product/update', methods=['GET', 'POST'])
 def productUpdate():
     if request.method == 'GET':
-        #if ("employee" in request.cookies) & (request.cookies["employee"] == "True"):
-        return render_template("update_productinfo.html")
-        #else:
-        #   return "FORBIDDEN"
+        if ("employee" in request.cookies) & (request.cookies["employee"] == "True"):
+            return render_template("update_productinfo.html")
+        else:
+            return "FORBIDDEN"
     if request.method == 'POST':
         payload = request.form
-        userid = None
         cursor = connection.cursor()
-        querystr = '''
-                    select user_id from account
-                    where username = {0}
-                    and password = {1}
-                '''.format(payload["inputUsername"], payload["inputPassword"])
-        cursor.execute(querystr)
-        for id in cursor:
-            userid = id
+        querystr = '''update product
+                        set product_name = :name,
+                        product_price = :price,
+                        upc_code = :UPC
+                        where product_id = :pid    
+                '''
+        cursor.execute(querystr, {"pid": payload["inputproductID"],
+                                  "UPC": payload["inputproductCode"],
+                                  "name": payload["inputName"],
+                                  "price": payload["inputPrice"]})
+        connection.commit()
         redirect_to_main = redirect('/main')
         response = app.make_response(redirect_to_main)
         response.set_cookie('auth_user', value='')  # Add the username arg
         response.set_cookie('employee', value='True')
+        return response
 
 
 if __name__ == "__main__":
